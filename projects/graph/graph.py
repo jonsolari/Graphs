46186a8e1b4afdff24cf2c5ae9e845576ec20c9b
System Administrator <jon.s.solari@gmail.com>
2020-02-17 17:45:07 -0500

    :art: mvp
    mvp

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -175,7 +175,7 @@ class Graph:
                 for neighbor in self.get_neighbors(v[-1]):
                     stack.push(path + [neighbor])
 
-    def dfs_recursive(self, starting_vertex, destination_vertex):
+    def dfs_recursive(self, starting_vertex, destination_vertex, path=[], visited=None):
         """
         Return a list containing a path from
         starting_vertex to destination_vertex in
@@ -183,6 +183,23 @@ class Graph:
 
         This should be done using recursion.
         """
+        
+        if visited is None:
+            visited = set()
+
+        visited.add(starting_vertex)
+
+        path = path + [starting_vertex]
+
+        if starting_vertex == destination_vertex:
+            return path
+ 
+        for neighbor in self.get_neighbors(starting_vertex):
+            if neighbor not in visited:
+                path2 = self.dfs_recursive(neighbor, destination_vertex, path, visited)
+                if path2 is not None:
+                    return path2
+
        
 
 if __name__ == '__main__':
